eeebfc8241e684fb98e871f138883eb88aa1468f
Eric Kernfeld <eric.kern13@gmail.com>
2022-06-02 09:21:08 -0400

    Work out ill-conceived regression demo

--- course content/6_be_conservative/linear_regression_demo.R
+++ course content/6_be_conservative/linear_regression_demo.R
@@ -1,34 +1,40 @@
 # Q1
+set.seed(1)
 regression_demo = data.frame(
   x1 = sample(rep(1:10, times = 5)), # the sample() function puts things in a random order.
   x2 = sample(rep(1:10, times = 5)),
   x3 = sample(rep(1:10, times = 5)) 
 )
 regression_demo[["y"]] = regression_demo[["x1"]]*1.1 + regression_demo[["x2"]]*0.1 + rbinom(nrow(regression_demo), 10, 0.5)
-plot(regression_demo$y, regression_demo$x1)
-plot(regression_demo$y, regression_demo$x2)
+plot(regression_demo$x1, regression_demo$y)
+plot(regression_demo$x2, regression_demo$y)
+plot(regression_demo$x3, regression_demo$y)
 model_complex = lm(y ~ 1 + x1 + x2 + x3, data = regression_demo)
 model_complex
 summary(model_complex)
 
 # Q2
-number_of_simulations = 1e2
-number_of_simulations_inner = 1e2
-yz_association_complex = yz_association_simple = rep(NA, number_of_simulations)
+number_of_simulations = 1e4
+yz_association_true_model = yz_association_complex_model = yz_association_simple_model = rep(NA, number_of_simulations)
 for( j in 1:number_of_simulations){
   # z looks a lot like y, but only contains info from x1 and x2. 
+  regression_demo[["y"]] = regression_demo[["x1"]]*1.1 + regression_demo[["x2"]]*0.1 + rbinom(nrow(regression_demo), 10, 0.5)
   regression_demo[["z"]] = regression_demo[["x1"]]*1.1 + regression_demo[["x2"]]*0.1 + rbinom(nrow(regression_demo), 10, 0.5)
+  # Make a negative control that looks like y, but only contains info from x1 and x2. 
+  # Will it be good enough?
   model_simple = lm(y ~ 1 + x1, data = regression_demo)
-  model_simple
-  yz_association_complex_inner = yz_association_simple_inner = rep(NA, number_of_simulations_inner)
-  for(i in 1:number_of_simulations){
-    regression_demo[["y_predicted_complex"]] = predict(model_complex) + rbinom(nrow(regression_demo), 10, 0.5)
-    regression_demo[["y_predicted_simple"]] = predict(model_simple) + rbinom(nrow(regression_demo), 10, 0.5)
-    yz_association_complex_inner[i] = with(regression_demo, cor(z, y) - cor(z, y_predicted_complex))
-    yz_association_simple_inner[i] = with(regression_demo, cor(z, y) - cor(z, y_predicted_simple))
-  }
-  yz_association_simple[j] = mean(yz_association_simple_inner)
-  yz_association_complex[j] = mean(yz_association_complex_inner)
+  model_complex = lm(y ~ 1 + x1 + x2 + x3, data = regression_demo)
+  regression_demo[["negative_control_complex_model"]] = predict(model_complex) + rbinom(nrow(regression_demo), 10, 0.5)
+  regression_demo[["negative_control_simple_model"]] = predict(model_simple) + rbinom(nrow(regression_demo), 10, 0.5)
+  regression_demo[["negative_control_true_model"]] = regression_demo[["x1"]]*1.1 + regression_demo[["x2"]]*0.1 + rbinom(nrow(regression_demo), 10, 0.5)
+  #  For predicting z, is the negative control just as good as y?
+  yz_association_true_model[j] = with(regression_demo, cor(z, y) - cor(z, negative_control_true_model))
+  yz_association_complex_model[j] = with(regression_demo, cor(z, y) - cor(z, negative_control_complex_model))
+  yz_association_simple_model[j] = with(regression_demo, cor(z, y) - cor(z, negative_control_simple_model))
 }
-hist(yz_association_simple)
-hist(yz_association_complex)
+hist(yz_association_simple_model, 40)
+hist(yz_association_complex_model, 40)
+hist(yz_association_true_model, 40)
+mean(yz_association_simple_model)
+mean(yz_association_complex_model)
+mean(yz_association_true_model)
